36fe4a8da8650d745384bc481bfd9f11497161a5
Endrophes <Endrophes@users.noreply.github.com>
2023-02-24 15:55:38 -0500

    Update Dice.gd
    * Random number on role.
    * Dice change texture to match the number
    * Added get value

--- Game/src/Dice.gd
+++ Game/src/Dice.gd
@@ -1,6 +1,14 @@
 extends Node2D
 class_name Dice
 
+# Preload
+var die1 = preload("res://assets/Images/dieWhite1.png")
+var die2 = preload("res://assets/Images/dieWhite2.png")
+var die3 = preload("res://assets/Images/dieWhite3.png")
+var die4 = preload("res://assets/Images/dieWhite4.png")
+var die5 = preload("res://assets/Images/dieWhite5.png")
+var die6 = preload("res://assets/Images/dieWhite6.png")
+
 # Declare member variables here. Examples:
 var value = 1;
 var isLocked = false;
@@ -27,4 +35,35 @@ func _button_pressed():
 	print(alphaLevel)
 	
 func _roll():
-	print("Random BS GO!!!")
+	if !isLocked:
+		value = 0;
+		while value == 0:
+			value = randi() % 6;
+#		print("New Value:", value);
+		_changeSprite();
+	else:
+#		print("Nope");
+		return;
+
+func _changeSprite():
+	var newTexture = die1;
+	
+	match (value):
+		1 :
+			newTexture = die1;
+		2 :
+			newTexture = die2;
+		3 :
+			newTexture = die3;
+		4 :
+			newTexture = die4;
+		5 :
+			newTexture = die5;
+		6:
+			newTexture = die6;
+	
+	var dieSprite = get_node("Die");
+	dieSprite.set_texture(newTexture);
+
+func _getValue():
+	return value;
